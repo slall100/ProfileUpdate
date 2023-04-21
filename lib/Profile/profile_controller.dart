fc6552f3ddd8f1d69f6d34c6e46cd70722fff7e1
slall100 <127601732+slall100@users.noreply.github.com>
2023-04-21 18:33:39 +0200

    Add files via upload

---
+++ lib/Profile/profile_controller.dart
@@ -0,0 +1,40 @@
+mport 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileController with ChangeNotifier {
+
+  void pickImage(context){
+    showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return AlertDialog(
+            content: Container(
+              height: 120,
+              child : Column(
+                children: [
+                  ListTile(
+                    onTap: () {
+
+                    },
+                    leading: Icon(Icons.camera, color: Colors.white,),
+                    title: Text("Camera"),
+
+                  ),
+                  ListTile(
+                    onTap: () {
+
+                    },
+                    leading: Icon(Icons.image, color: Colors.white,),
+                    title: Text("Gallery"),
+
+                  )
+                ],
+              )
+
+
+            ),
+          );
+        }
+    );
+}
+}
